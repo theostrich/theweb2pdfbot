a9f414d22d5afce66206846b18919a4f0a7c9a56
෴ 乂(≧▽≦) 乂 ෴ <75663262+NoOneLuvMe@users.noreply.github.com>
2021-05-17 17:13:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pdfkit
 import telegram
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler,MessageHandler, Filters
+from telegram import MessageEntity
 import os
 
 TOKEN = os.getenv("TOKEN")
@@ -29,8 +30,8 @@ def start(update, context):
     message = f'''
 <b>Hey @{username} 👋
  
-I can convert a WebPage To Pdf 📄
-/help to for help 
+🌀 I can convert a WebPage To Pdf 
+💀 /help to for help 
 </b>
     '''
     context.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode='html',
@@ -41,16 +42,11 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 
-# help
+# Help
 def help(update, context):
     username = update.message.chat.username
     print("Help :", username)
-    message = '''
-<b>
-Usage:
-/web2pdf {url}
-example </b> : /web2pdf https://theostrich.eu.org/
-    '''
+    message = '''<b>Just Send Me A Link Dude🥱</b> '''
     context.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode='html'
                              , disable_web_page_preview=True)
 
@@ -62,20 +58,24 @@ dispatcher.add_handler(help_handler)
 # web2pdf
 
 def web2pdf(update, context):
-    print("web2pdf : ", update.message.chat.username)
-    url = context.args[0]
-    print("url = ", url)
-    cap=f"🔗 Url : {url}"
-    if "http" in url:
-            pdfkit.from_url(url, "@theweb2pdfbot.pdf")
+        print("web2pdf : ", update.message.chat.username)
+        url=update.message.text
+        print("url = ", url)
+        if "http" in url:
+            keyboard = [
+                [
+                    telegram.InlineKeyboardButton("Url 🔗",
+                                                  url=url),
+                ]
+            ]
+            reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+            config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
+            pdfkit.from_url(url, output_path="@theweb2pdfbot.pdf", configuration=config)
+
             context.bot.send_document(chat_id=update.effective_chat.id,
-                                      document=open('@theweb2pdfbot.pdf', 'rb'),
-                                      caption=cap, parse_mode="html",timeout=150)
-    else:
-        update.message.reply_text(reply_to_message_id=update.message.message_id, text="Send A valid 🔗 url")
+                                      document=open('@theweb2pdfbot.pdf', 'rb'), parse_mode="html", reply_markup=reply_markup,timeout=200)
 
-
-web2pdf_handler = CommandHandler('web2pdf', web2pdf)
+web2pdf_handler = MessageHandler(Filters.text & Filters.entity(MessageEntity.URL), web2pdf)
 dispatcher.add_handler(web2pdf_handler)
 
 updater.start_polling()
